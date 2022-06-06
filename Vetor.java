b49c9af72762d270cf8e5d9b332a1d677196ab3e
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-06-06 17:12:27 +0000

    Vetor

---
+++ Vetor.java
@@ -0,0 +1,16 @@
+public class Vetor{
+    public static void main(String[] args) {
+        int vetor[];
+        vetor= new int[5];
+        vetor[0]=1;
+        vetor[1]=5;
+        vetor[2]=9;
+        vetor[3]=8;
+        vetor[4]=6;
+
+        for(int i=0; i<(vetor.length); i++){
+            System.out.println(vetor[i]);
+        }
+
+    }
+}
